5708d3b0ab26109006220aa7d36a67ecd19a7207
ceefit <ceefit@protonmail.com>
2018-07-02 22:06:37 -0400

    Remove black flicker when not using slideshow

--- wiki-learn/__main__.py
+++ wiki-learn/__main__.py
@@ -1,23 +1,15 @@
 import argparse
-from slideshow import render_slideshow
-
 from wikipedia import wikipedia
-
-
-global sprite
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--slideshow', help='Render a slideshow of the images found on that subject',
                     action='store_true')
-
-
 parser.add_argument('wikipage', help='Wiki page title',
                     nargs='?', default="Iron Man")
-
 args = parser.parse_args()
 
 if args.slideshow:
+    from slideshow import render_slideshow
     render_slideshow(args.wikipage)
 else:
     print(wikipedia.summary(args.wikipage))
